f5eeba51629ad8ef17ff8bcbfb85acf9b80fcee6
Soloquie <cesarcamilotulcanerira@gmail.com>
2026-04-05 10:58:08 -0500

    Apuntar api a Cloudfront

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://Cinemax-backend-env.eba-qvyazdgq.us-east-2.elasticbeanstalk.com'
+  apiBaseUrl: 'https://das7x60ahkyu.cloudfront.net'
 };
